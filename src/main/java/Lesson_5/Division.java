2fa15d0cc686a747413221cbd6127e4d4db7288b
Ivan Ionov <IAIonov.SBT@sberbank.ru>
2018-03-19 13:12:36 +0300

    Initial commit

---
+++ src/main/java/Lesson_5/Division.java
@@ -0,0 +1,11 @@
+package Lesson_5;
+
+/**
+ * Класс "Деление"
+ */
+public class Division extends Operation {
+	@Override
+	double getResult(double a, double b) {
+		return a/b;
+	}
+}
---
+++ src/main/java/Lesson_5/Main.java
@@ -0,0 +1,33 @@
+package Lesson_5;
+
+public class Main {
+	public static void main(String args[]){
+
+		System.out.print("Введите первое число: ");
+		double a = Input.doubleNum();
+
+		System.out.print("Введите второе число: ");
+		double b = Input.doubleNum();
+
+		System.out.print("Выберите операцию (+,-,*,/): ");
+		char oper = Input.operation();
+
+		System.out.println("Вывод:");
+		Operation calc;
+
+		switch (oper){
+			case '+': calc = new Addition();
+			System.out.printf("%.4f %s %.4f %s %.4f",a,oper,b," = ",calc.getResult(a,b));
+			break;
+			case '-': calc = new Subtraction();
+			System.out.printf("%.4f %s %.4f %s %.4f",a,oper,b," = ",calc.getResult(a,b));
+			break;
+			case '*': calc = new Multiplication();
+			System.out.printf("%.4f %s %.4f %s %.4f",a,oper,b," = ",calc.getResult(a,b));
+			break;
+			case '/': calc = new Division();
+			System.out.printf("%.4f %s %.4f %s %.4f",a,oper,b," = ",calc.getResult(a,b));
+			break;
+		}
+	}
+}
